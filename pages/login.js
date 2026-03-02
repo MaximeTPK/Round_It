39e8b2d63f0844de709f5fb63bb268842f6b5447
MaximeTPK <maxime.poli@thepackengers.com>
2026-03-02 21:03:32 +0100

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -10,7 +10,10 @@ export default function Login() {
 
   useEffect(() => {
     const { createClient } = require('@supabase/supabase-js')
-    if (!window.__sb) window.__sb = createClient('https://yqjenhpaohwunjvgmlyw.supabase.co', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '')
+    if (!window.__sb) window.__sb = createClient(
+  'https://yqjenhpaohwunjvgmlyw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxamVuaHBhb2h3dW5qdmdtbHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0MTM1MTYsImV4cCI6MjA4Nzk4OTUxNn0.-81H9_nbaNJitTCJmVAJxE_l3FIio3algjCJGjovUcs'
+)
     setClient(window.__sb)
   }, [])
 
