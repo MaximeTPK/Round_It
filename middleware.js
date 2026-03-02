7de5af5c2e2de4c878dd98fac0a7d962bcc4a9a5
MaximeTPK <maxime.poli@thepackengers.com>
2026-03-02 21:37:50 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,23 +1,2 @@
-import { NextResponse } from 'next/server'
-
-export function middleware(req) {
-  const { pathname } = req.nextUrl
-
-  if (
-    pathname.startsWith('/api/') ||
-    pathname.startsWith('/_next/') ||
-    pathname === '/login' ||
-    pathname === '/register' ||
-    pathname === '/favicon.ico' ||
-    pathname === '/favicon.png'
-  ) return NextResponse.next()
-
-  const token = req.cookies.get('sb-yqjenhpaohwunjvgmlyw-auth-token')
-  if (token) return NextResponse.next()
-
-  return NextResponse.redirect(new URL('/login', req.url))
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image).*)'],
-}
+export function middleware() {}
+export const config = { matcher: [] }
