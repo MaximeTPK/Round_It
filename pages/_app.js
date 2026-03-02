071de31248ede5547fedd4b65aeea4b78605b522
MaximeTPK <maxime.poli@thepackengers.com>
2026-03-02 20:24:40 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,15 @@
-import '../styles/globals.css'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
 export default function App({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
   return <Component {...pageProps} />
 }
